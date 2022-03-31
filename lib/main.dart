c979bb513c757021483180db550738eef2a2be85
shramitgupta <shramitgupta1212@gmail.com>
2022-03-31 22:24:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text("Welcome to 30 day flutter"),
+        ),
+      ),
+    );
+  }
+}
